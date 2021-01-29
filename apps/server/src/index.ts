c16de1cb4d2ee6575ad10efe651ecb4b46920adf
Matt Hayes <matt@mysterycommand.com>
2021-01-29 00:45:42 -0600

    feat: a dev server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,18 @@
 import { createServer } from 'http';
+import Ws from 'ws';
+
+const server = createServer((req, res) => {
+  console.log(req.url);
+  res.writeHead(200);
+  res.end('<p>hi, hello</p>', 'utf-8');
+});
+const wss = new Ws.Server({ server });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log(`received: ${message}`);
+    ws.send(message);
+  });
+});
+
+server.listen(8080);
